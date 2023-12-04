1ba96b70b869909795961bdcec7e91aa9c863cb3
Nico Fehr <nico.fehr@ost.ch>
2023-12-04 18:56:22 +0100

    Add date selector to new event dialog

--- app/src/main/java/ch/ost/gartenzwergli/ui/home/NewEventDialog.kt
+++ app/src/main/java/ch/ost/gartenzwergli/ui/home/NewEventDialog.kt
@@ -1,7 +1,9 @@
 package ch.ost.gartenzwergli.ui.home
 
+import android.app.DatePickerDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +11,8 @@ import android.view.ViewGroup
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.DialogFragment
 import ch.ost.gartenzwergli.R
+import ch.ost.gartenzwergli.databinding.FragmentNewEventDialogBinding
+import java.util.Calendar
 
 /**
  * A simple [Fragment] subclass.
@@ -18,6 +22,14 @@ import ch.ost.gartenzwergli.R
 class NewEventDialog : DialogFragment() {
 
     private var _toolbar: Toolbar? = null
+    private var _binding: FragmentNewEventDialogBinding? = null
+
+    private val binding get() = _binding!!
+
+    private val calendar: Calendar = Calendar.getInstance()
+    private val dateFormat = "dd.MM.yyyy"
+    private val simpleDateFormat = java.text.SimpleDateFormat(dateFormat, java.util.Locale.GERMANY)
+
     var toolbar: Toolbar?
         get() = _toolbar
         set(value) {
@@ -33,9 +45,17 @@ class NewEventDialog : DialogFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val root: View = inflater.inflate(R.layout.fragment_new_event_dialog, container, false)
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        _binding = FragmentNewEventDialogBinding.inflate(inflater, container, false)
+        val root = binding.root
         toolbar = root.findViewById(R.id.toolbar)
+
+        binding.newEventCalendarText.text = Editable.Factory
+            .getInstance().newEditable(simpleDateFormat.format(calendar.time))
+        binding.newEventCalendarText.setOnClickListener {
+            showDatePickerDialog()
+        }
 
         return root;
     }
@@ -69,5 +89,31 @@ class NewEventDialog : DialogFragment() {
             dialog.getWindow()?.setLayout(width, height)
             dialog.getWindow()?.setWindowAnimations(R.style.Theme_Gartenzwergli)
         }
+    }
+
+    private fun showDatePickerDialog() {
+        // get current date
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+
+        // create date picker dialog
+        val datePickerDialog: DatePickerDialog = DatePickerDialog(
+            requireContext(),
+            { view, year, month, dayOfMonth ->
+                // set day of month , month and year value in the edit text
+                binding.newEventCalendarText.setText("" + dayOfMonth + "." + (month + 1) + "." + year)
+            },
+            year,
+            month,
+            dayOfMonth
+        )
+
+        datePickerDialog.show()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
