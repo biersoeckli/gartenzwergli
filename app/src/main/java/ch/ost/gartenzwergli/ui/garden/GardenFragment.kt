5e1f46b9501da6dedbd921c6e015fdc653eb25b0
Nico Fehr <nico.fehr@ost.ch>
2023-12-05 15:33:08 +0100

    Add garden fragment

--- app/src/main/java/ch/ost/gartenzwergli/ui/garden/GardenFragment.kt
+++ app/src/main/java/ch/ost/gartenzwergli/ui/garden/GardenFragment.kt
@@ -7,8 +7,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import ch.ost.gartenzwergli.R
+import ch.ost.gartenzwergli.databinding.FragmentGardenBinding
+import ch.ost.gartenzwergli.databinding.FragmentHomeBinding
+import com.google.android.material.snackbar.Snackbar
 
 class GardenFragment : Fragment() {
+
+    private var _binding: FragmentGardenBinding? = null
+
+    private val binding get() = _binding!!
 
     companion object {
         fun newInstance() = GardenFragment()
@@ -20,7 +27,30 @@ class GardenFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_garden, container, false)
+        _binding = FragmentGardenBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        binding.gardenToolbar.inflateMenu(R.menu.menu_garden)
+
+        binding.gardenToolbar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.garden_settings -> {
+                    // Handle favorite icon press
+                    Snackbar.make(root, "Settings", Snackbar.LENGTH_SHORT).show()
+                    true
+                }
+
+                else -> {
+                    false
+                }
+            }
+        }
+
+        binding.addPlantFab.setOnClickListener { view ->
+
+        }
+
+        return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
